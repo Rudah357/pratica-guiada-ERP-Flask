76d32ff481cf298f76dbbdfdf010300d20e1d626
CAUA RICARDO FLORES <05796690094@senacrs.edu.br>
2026-06-22 20:05:13 -0300

    pratica guiada

--- app/routes/web_bp.py
+++ app/routes/web_bp.py
@@ -8,12 +8,13 @@ web_bp = Blueprint("web", __name__)
 def index():
     return redirect(url_for("web.listar_produtos_view"))
 
+# ROTAS DE PRODUTOS ====================================================================
 
 @web_bp.route("/produtos")
 def listar_produtos_view():
     produtos = produtos_controller.listar_todos_produtos()
     return render_template("produtos/listar.html", produtos=produtos)
-    
+
 @web_bp.route("/produto/novo", methods=["GET", "POST"])
 def novo_produto_view():
     categorias = categoria_controller.listar_todas_categorias()
@@ -27,6 +28,48 @@ def novo_produto_view():
         flash(msg, "success" if sucesso else "danger")
 
         if sucesso:
-            return redirect(url_for('web.listar_produtos_view'))
+            return redirect(url_for("web.listar_produtos_view"))
         
     return render_template("produtos/form.html", produto=None, categorias=categorias)
+
+@web_bp.route("/produtos/editar/<int:id>", methods=["GET", "POST"])
+def editar_produto_view(id):
+    pass
+
+
+@web_bp.route("/produtos/excluir/<int:id>", methods=["DELETE"])
+def excluir_produto_view(id):
+    pass
+
+
+# ROTAS DE CATEGORIAS ====================================================================
+
+@web_bp.route("/categorias")
+def listar_categorias_view():
+    categorias = categoria_controller.listar_todas_categorias()
+    return render_template("categorias/listar.html", categorias=categorias)
+    
+
+@web_bp.route("/categorias/novo", methods=["GET", "POST"])
+def nova_categoria_view():
+    if request.method == "POST":
+        nome = request.form.get("nome_categoria")
+        
+        sucesso, msg = categoria_controller.salvar_categoria(nome)
+
+        flash(msg, "success" if sucesso else "danger")
+
+        if sucesso:
+            return redirect(url_for('web.listar_categorias_view'))
+
+    return render_template("categorias/form.html", categoria=None)
+
+
+@web_bp.route("/categorias/editar/<int:id>", methods=["GET", "POST"])
+def editar_categoria_view(id):
+    pass
+
+
+@web_bp.route("/categorias/excluir/<int:id>", methods=["DELETE"])
+def excluir_categoria_view(id):
+    pass
